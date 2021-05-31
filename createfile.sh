5680b91eabcaeadc8a5c989de557bba71e6b88e0
poojagurubasappa@gmail.com <poojagurubasappa@gmail.com>
2021-05-31 10:58:24 +0530

    variables part1

---
+++ createfile.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+echo "Enter the name of your file"
+read MY_FILE_NAME
+echo "Creating a file ${MY_FILE_NAME}_file"
+touch "${MY_FILE_NAME}_file"
---
+++ var.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+MY_MESSAGE="hello world"  #there should be no space in the assignment to variables
+echo $MY_MESSAGE
