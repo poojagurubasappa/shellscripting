03c711398f9be07e6c01584470add5c8bb42078e
Pooja <in22907823@global.tesco.org>
2021-05-31 09:39:14 +0530

    first command

---
+++ first.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a comment
+echo Hello World  #echo is a command with 2 parameters "Hello" and "World"
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
